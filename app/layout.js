35da0c9c1f191868c3b018e3f7a2f4d645db2c31
Farhaat Mohammad <36752934+Farhaat11@users.noreply.github.com>
2025-07-10 13:52:37 +0530

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -23,9 +23,7 @@ export default function RootLayout({ children }) {
         <Toaster richColors  />
         {/* footer */}
         <footer className="bg-blue-50 py-12">
-          <div className="container mx-auto px-4 text-center text-gray-600">
-            <p>Made By Arjun Bothra</p>
-          </div>
+         
         </footer>
       </body>
     </html>
